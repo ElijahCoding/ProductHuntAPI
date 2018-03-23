59f3dfa2c82bb376c0337374cc014c3cd933c368
Elijah <elijahsharper1@gmail.com>
2018-03-23 22:31:42 +0300

    Console Command

--- routes/api.php
+++ routes/api.php
@@ -3,3 +3,15 @@
 use Illuminate\Http\Request;
 
 Route::get('producthunt', 'Api\HomeController@producthunt');
+
+Route::get('/', function() {
+  $client = new \GuzzleHttp\Client();
+
+  $response = $client->request('GET', 'https://jsonplaceholder.typicode.com/posts');
+
+  $response =  json_decode($response->getBody());
+
+  foreach ($response as $post) {
+    dump($post);
+  }
+});
